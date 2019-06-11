7a4ccf85c22ab98ed7a993146c67d39b5601e620
liqiang <jdswee@163.com>
2019-06-11 17:18:31 +0800

    doing-电子书解析

---
+++ src/Ebook.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>ebook</div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+export default {
+  methods: {
+    // 电子书的解析和渲染
+    showEpub () {
+      // 生成 Book
+      this.book = new Epub(DOWNLOAD_URL)
+      console.log(this.book)
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  @import 'assets/styles/global.scss';
+</style>

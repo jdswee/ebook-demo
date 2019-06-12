078da429c73c49c90ca51aed6c201c38d4e2a33a
liqiang <jdswee@163.com>
2019-06-12 17:26:38 +0800

    标题栏和菜单栏实现(上)

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>ebook</div>
+  <div class="ebook">
+    <div class="title-wrapper">
+      <div class="left">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-person icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-more icon"></span>
+        </div>
+      </div>
+    </div>
+    <div class="read-wrapper">
+      <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
+    </div>
+    <!-- 菜单栏 -->
+    <div class="menu-wrapper">
+      <div class="icon-wrapper">
+        <span class="icon-menu icon"></span>
+      </div>
+      <div class="icon-wrapper">
+        <span class="icon-progress icon"></span>
+      </div>
+      <div class="icon-wrapper">
+        <span class="icon-bright icon"></span>
+      </div>
+      <div class="icon-wrapper">
+        <span class="icon-share icon"></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +48,29 @@ const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 export default {
   methods: {
     // 电子书的解析和渲染
-    showEpub () {
+    showEpub() {
       // 生成 Book
       this.book = new Epub(DOWNLOAD_URL)
-      console.log(this.book)
+      // 通过 Book.renderTo 生成 Rendition
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过 Rendition.display 渲染电子书
+      this.rendition.display()
+    },
+    prevPage() {
+      if(this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage() {
+      if(this.rendition) {
+        this.rendition.next()
+      }
     }
   },
-  mounted () {
+  mounted() {
     this.showEpub()
   }
 }
@@ -22,4 +78,66 @@ export default {
 
 <style lang='scss' scoped>
   @import 'assets/styles/global.scss';
+  .ebook {
+    position: relative;
+    .title-wrapper {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 101;
+      display: flex;
+      width: 100%;
+      height: px2rem(48);
+      background: white;
+      box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
+      .left {
+        flex: 0 0 px2rem(60);
+        @include center;
+      }
+      .right {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        .icon-wrapper {
+          flex: 0 0 px2rem(40);
+          @include center;
+        }
+      }
+    }
+    .read-wrapper {
+      .mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 100;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        .left {
+          flex: 0 0 px2rem(100);
+        }
+        .center {
+          flex: 1;
+        }
+        .right {
+          flex: 0 0 px2rem(100);
+        }
+      }
+    }
+    .menu-wrapper {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      z-index: 101;
+      display: flex;
+      width: 100%;
+      height: px2rem(48);
+      background: white;
+      box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+      .icon-wrapper {
+        flex: 1;
+        @include center;
+      }
+    }
+  }
 </style>

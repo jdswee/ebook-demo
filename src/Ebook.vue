8075126e107d55f00bc5a84820ce55fd42f5cf96
liqiang <jdswee@163.com>
2019-06-13 17:08:41 +0800

    字号设置功能实现(上)

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="ebook">
-    <transition name="slide-down">
-      <div class="title-wrapper" v-show="ifTitleAndMenuShow">
-        <div class="left">
-          <span class="icon-back icon"></span>
-        </div>
-        <div class="right">
-          <div class="icon-wrapper">
-            <span class="icon-cart icon"></span>
-          </div>
-          <div class="icon-wrapper">
-            <span class="icon-person icon"></span>
-          </div>
-          <div class="icon-wrapper">
-            <span class="icon-more icon"></span>
-          </div>
-        </div>
-      </div>
-    </transition>
+    <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
     <div class="read-wrapper">
       <div id="read"></div>
       <div class="mask">
@@ -26,31 +9,21 @@
         <div class="right" @click="nextPage"></div>
       </div>
     </div>
-    <!-- 菜单栏 -->
-    <transition name="slide-up">
-      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
-        <div class="icon-wrapper">
-          <span class="icon-menu icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-progress icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-bright icon"></span>
-        </div>
-        <div class="icon-wrapper">
-          <span class="icon-share icon"></span>
-        </div>
-      </div>
-    </transition>
+    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" ref="menuBar"></menu-bar>
   </div>
 </template>
 
 <script>
 import Epub from 'epubjs'
+import TitleBar from '@/components/TitleBar'
+import MenuBar from '@/components/MenuBar'
 const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 
 export default {
+  components: {
+    TitleBar,
+    MenuBar
+  },
   data() {
     return {
       ifTitleAndMenuShow: false
@@ -59,6 +32,9 @@ export default {
   methods: {
     toggelTitleMenuShow() {
       this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if(!this.ifTitleAndMenuShow) {
+        this.$refs.menuBar.hideSetting()
+      }
     },
     // 电子书的解析和渲染
     showEpub() {
@@ -93,30 +69,7 @@ export default {
   @import 'assets/styles/global.scss';
   .ebook {
     position: relative;
-    .title-wrapper {
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 101;
-      display: flex;
-      width: 100%;
-      height: px2rem(48);
-      background: white;
-      box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
-      .left {
-        flex: 0 0 px2rem(60);
-        @include center;
-      }
-      .right {
-        flex: 1;
-        display: flex;
-        justify-content: flex-end;
-        .icon-wrapper {
-          flex: 0 0 px2rem(40);
-          @include center;
-        }
-      }
-    }
+
     .read-wrapper {
       .mask {
         position: absolute;
@@ -137,20 +90,6 @@ export default {
         }
       }
     }
-    .menu-wrapper {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      z-index: 101;
-      display: flex;
-      width: 100%;
-      height: px2rem(48);
-      background: white;
-      box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
-      .icon-wrapper {
-        flex: 1;
-        @include center;
-      }
-    }
+
   }
 </style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- 菜单栏 -->
+  <div class="menu-bar">
+    <transition name="slide-up">
+      <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+        <div class="icon-wrapper">
+          <span class="icon-menu icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper">
+          <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper" @click="showSetting">
+          <span class="icon-a icon">A</span>
+        </div>
+      </div>
+    </transition>
+    <transition name="slide-up">
+      <div class="setting-wrapper" v-show="ifSettingShow">
+        <div class="setting-font-size"></div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      ifSettingShow: false
+    }
+  },
+  methods: {
+    showSetting() {
+      this.ifSettingShow = true
+    },
+    hideSetting() {
+      this.ifSettingShow = false
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/global';
+.menu-bar {
+  .menu-wrapper {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: px2rem(48);
+    background: white;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    .icon-wrapper {
+      flex: 1;
+      @include center;
+    }
+  }
+  .setting-wrapper {
+    position: absolute;
+    bottom: px2rem(48);
+    left: 0;
+    width: 100%;
+    height: px2rem(60);
+    background: #fff;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+    .setting-font-size {
+
+    }
+  }
+}
+</style>
